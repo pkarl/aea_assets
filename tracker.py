4bf975834de51e3eb7ba72bcd87802a6fd20a6fb
Pete Karl II <pete.karl@gmail.com>
2011-12-16 15:28:03 -0500

    A mess of changes, and no, this commit message will not help you

--- tracker.py
+++ tracker.py
@@ -8,13 +8,18 @@
     
     I've made username + password + embedly key into command line params because I don't
     want to deal with config files right now.
-
+    
+    1) change index format
+    3) add 'reach' in the form of a list of {'username' : follower_count } dicts
+    
 """
 
-import sys, pycurl, urllib, urllib2, redis
+import sys
+import pycurl
+import urllib
+import redis
 from embedly import Embedly
 import simplejson as json
-
 
 """
     Absorb & handle args for the script
@@ -27,69 +32,64 @@ if len(sys.argv) is not 4:
     Handle nozzling, storage + Embedly
 """
 
-ACCEPTED_ARGS = ['maxwidth', 'maxheight', 'format']
-
-def get_oembed(url, **kwargs):
-    """
-    Example Embedly oEmbed Function
-    """
-    api_url = 'http://api.embed.ly/1/oembed?'
-
-    params = {'url': url , 'key': sys.argv[3] }
-
-    for key, value in kwargs.items():
-        if key not in ACCEPTED_ARGS:
-            raise ValueError("Invalid Argument %s" % key)
-        params[key] = value
-
-    oembed_call = "%s%s" % (api_url, urllib.urlencode(params))
-
-    return json.loads(urllib2.urlopen(oembed_call).read())
-
-def attach_nozzle(api, callback, args):
+def attach_nozzle(callback, args):
     conn = pycurl.Curl()
-    conn.setopt(pycurl.USERPWD, "%s:%s" % (sys.argv[1], sys.argv[2]))
-    conn.setopt(pycurl.URL, "https://stream.twitter.com/1/statuses/%s.json" % api)
-    
-    conn.setopt(pycurl.WRITEFUNCTION, callback)
-
     data = urllib.urlencode(args)
+    
+    conn.setopt(pycurl.USERPWD, "%s:%s" % (sys.argv[1], sys.argv[2]))
+    conn.setopt(pycurl.URL, "https://stream.twitter.com/1/statuses/filter.json")
+    conn.setopt(pycurl.WRITEFUNCTION, callback)
     conn.setopt(pycurl.POSTFIELDS, data)
 
-    conn.perform()
+    try:
+        conn.perform()
+        print 'attached'
+    except pycurl.error, error:
+        errno, errstr = error
+        print 'ERROR -> 0: ', errstr
+    except Exception, err:
+        sys.stderr.write('ERROR -> 1: %s\n' % str(err))
 
 def hose(data):
     try:
         tweet = json.loads(data)
-        
-        for url in tweet['entities']['urls']:
-            
-            if r.hexists('urls', url['expanded_url']):
-                continue
-            try:
-                obj = get_oembed(url['expanded_url'])
-            except urllib2.HTTPError:
-                continue
-            
-            r.sadd('url_index', "%s:%s" % (r.get('current_index'), url['expanded_url']))
-            r.hset('urls', url['expanded_url'], obj)
-            r.incr('current_index')
-                
     except json.decoder.JSONDecodeError:
-        pass
-
+        return
+        
+    for url in tweet['entities']['urls']:
+        
+        if r.hexists('urls', url['expanded_url']):
+            continue
+        
+        print e.preview(url['expanded_url'], raw=True).raw
+        
+        # r.sadd('url_index', "%s:%s" % (r.get('current_index'), url['expanded_url']))
+        # r.hset('urls', url['expanded_url'], obj)
+        # r.incr('current_index')
 
 """
     Initialize the app
 """
 
+print 'Connecting to Redis... ',
+
+# create Redis connection
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 r.flushdb()
 if not r.exists('current_index'):
     r.set('current_index', 0)
-    
+
+print 'connected'
+
+print 'Creating Embedly client... ',
 e = Embedly(sys.argv[3])
+print 'created'
 
-args = { 'track': 'imgur,500px,flic.kr,#photo,#pic,yfrog' }
+print 'Testing Embedly endpoint... ',
+obj = e.preview('http://google.com/')
+if obj.error:
+    exit('embedly endpoint failed (http response %s)' % obj.error_code)
+print 'passed'
 
-attach_nozzle('filter', hose, args)
+print 'Attaching nozzle to Twitter firehose... '
+attach_nozzle(hose, { 'track': 'instagr' })
